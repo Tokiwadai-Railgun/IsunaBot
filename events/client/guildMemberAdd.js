2aa76c08aab3214308a3f466e4828c41644b3c70
Tokiwadai-Railgun <arthur.scheidbach@life.fr>
2020-07-15 14:16:27 +0200

    message bvn et goodbye Hazukumi

--- events/client/guildMemberAdd.js
+++ events/client/guildMemberAdd.js
@@ -4,4 +4,9 @@ module.exports = (Isuna, member) => {
 
     bvnchannel.send(`${member.displayName} à rejoin le serveur. YOUPI!!!!`);
   }
+  if (member.guild.id === '732692494621605909' ) {
+    const bvnchannel = member.guild.channels.cache.get("732692494621605913");
+
+    bvnchannel.send(`Bienvenue ${member} sur ${member.guild.displayName}. \n ||https://tenor.com/view/milim-nava-anime-demon-lord-happy-cute-gif-13644168||`)
+  }
 }
