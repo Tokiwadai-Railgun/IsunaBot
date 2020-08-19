6d14a0e3a5deec1817793cf2973d8324cbdb8c2e
Tokiwadai-Railgun <arthur.scheidbach@life.fr>
2020-08-19 09:52:02 +0200

    message de status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,9 @@
 module.exports = Isuna => {
-  if (!Isuna.channels.cache.find(ch => ch.name === 'isuna-log')) return;
+  if (!Isuna.channels.cache.find(ch => ch.name === 'izuna-log')) return;
   const isunaLog = Isuna.channels.cache.get('733580968534868090');
   if (!isunaLog) return;
   
+  Isuna.user.setActivity("izu help pour les commandes");
   isunaLog.send("Aucun probleme détécté lors du démarrage");
   console.log(`${Isuna.user.tag}dit : Je suis prête`);
 };
