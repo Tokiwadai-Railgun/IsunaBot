7e8e48a7db4252909d865bea1eff084c0469335e
Tokiwadai-Railgun <arthur.scheidbach@life.fr>
2020-08-22 08:59:47 +0200

    xp off sur heroku

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ Isuna.on("ready", () => {
   const isunaLog = Isuna.channels.cache.get('733580968534868090');
   if (!isunaLog) return;
   
-  Isuna.user.setActivity("/!\\ maintenance /!\\ izu help", { type: 'LISTENING' });
+  Isuna.user.setActivity("XP off | izu help", { type: 'LISTENING' });
   isunaLog.send("Aucun probleme détécté lors du démarrage");
   console.log(`${Isuna.user.tag}dit : Je suis prête`);
 });
