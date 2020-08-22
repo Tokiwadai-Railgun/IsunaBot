e3636fc11122d5f306b7d46d88213b4c92e152f0
Tokiwadai-Railgun <arthur.scheidbach@life.fr>
2020-08-22 08:54:59 +0200

    affichage de XP OFF sur heroku

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,7 +3,7 @@ module.exports = Isuna => {
   const isunaLog = Isuna.channels.cache.get('733580968534868090');
   if (!isunaLog) return;
   
-  Isuna.user.setActivity("/!\\ maintenance /!\\ izu help", { type: 'LISTENING' });
+  Isuna.user.setActivity("XP off | izu help", { type: 'LISTENING' });
   isunaLog.send("Aucun probleme détécté lors du démarrage");
   console.log(`${Isuna.user.tag}dit : Je suis prête`);
 };
